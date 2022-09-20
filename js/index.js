e4b837dd5acce720884122114dc47612ac5468a9
DiegoBorja09 <diegoborja09@gmail.com>
2022-09-19 20:55:15 -0500

    final

---
+++ js/index.js
@@ -0,0 +1,33 @@
+window.sr =ScrollReveal();
+
+sr.reveal('.main',{
+    duration:4000,
+    origin:'bottom',
+    distance:'-200px'
+
+})
+
+
+sr.reveal('.about',{
+    duration:4000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+sr.reveal('.services-reveal',{
+    duration:4000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+sr.reveal('.portafolio',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+
+
